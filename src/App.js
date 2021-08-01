38cd716aea563cbbdfbf098099794177710459c9
desh04 <singh.desh0404@gmail.com>
2021-08-01 22:35:52 +0530

    updating app with the ShopPage, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import { Switch , Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
+/* const HatsPage = () => (
   
   <div>
     <h1> HATS PAGE </h1>
   </div>
-  
-)
+  was using this only for the routing lesson demonstration 
+) */
 
 function App() {
   return (
     <div>
       <Switch>     
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        {/* <Route exact path="/hats" component={HatsPage} /> using for the demonstration perpose only*/}
+        <Route exact path='/shop' component={ShopPage} />
       </Switch> 
     </div>
   );
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item.component';
+
+import './collection-preview.component.style.scss';
+
+const CollectionPreview = ({ title, items }) => (
+    <div className="collection-preview">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <div className="preview">
+            {items
+                .filter((item, idx) => idx < 4)         //to show only four item in the preview section, rest will be shown when click on the title.
+                .map(({id, ...otherItemProps}) => (
+                <CollectionItem key={id} { ...otherItemProps }/>
+            ))}
+        </div>
+    </div>
+)
+
+export default CollectionPreview;
