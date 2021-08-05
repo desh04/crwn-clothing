060c09bc1548694bc8ab5c96b811ee922d795513
desh04 <singh.desh0404@gmail.com>
2021-08-05 20:38:34 +0530

    created sign-in form, custom Input and button component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 /* const HatsPage = () => (
   
@@ -23,6 +24,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         {/* <Route exact path="/hats" component={HatsPage} /> using for the demonstration perpose only*/}
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signIn' component={SignInAndSignUpPage} />
       </Switch> 
     </div>
   );
---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './form-input.style.scss';
+
+// making custom input and button component so can make consistent styling through out the webpage
+// these will be used in the sign-up option also . 
+// so making component so will not have to write the code again.
+// button component will be used outside the contact area also like in the cart, in shop page. 
+// so button component will be helpful
+
+const FormInput = ({ handleChange, label, ...otherProps}) => (
+    <div className='group'>
+        <input 
+            className='form-input' 
+            onChange={handleChange} {...otherProps}  // name type value required all will be passed in other props
+        />
+        {/*label prop will be used to selectivally render a label
+        if developer pass the label then this label prop will be created  */}
+        {   // if props.value exist then the shrink property will be added  
+            label ?
+            (<label 
+                className={`${otherProps.value.length ? "shrink" : ''} form-input-label`}
+            > 
+              { label }  
+            </label>)
+            : null
+        }
+        
+    </div>
+);
+
+export default FormInput;
