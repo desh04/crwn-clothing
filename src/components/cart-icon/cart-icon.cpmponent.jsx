332e7c1fcbd0e0f206f283b8f340857edd24eeb0
desh04 <singh.desh0404@gmail.com>
2021-10-10 13:21:51 +0530

    cart number connected to reducer

--- src/components/cart-icon/cart-icon.cpmponent.jsx
+++ src/components/cart-icon/cart-icon.cpmponent.jsx
@@ -7,18 +7,27 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.css';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon'onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
+// my try to increase the number in the cart
 
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce(function(acc, curr){
+        acc = acc + curr.quantity;
+        return acc;
+    }, 0)
+});
+
+// del above in case of problem
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
 export default connect(
-    null, 
+    mapStateToProps, 
     mapDispatchToProps
 )(CartIcon);
